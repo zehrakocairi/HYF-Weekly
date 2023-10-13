ecd462cbf14a25209bd5ca6639dc65963d4f67a1
zehra <zehrakocairi@gmail.com>
2023-10-14 00:06:58 +0200

    some fixes

--- Exercises/APIs/week2/pokemon-fetch/index.js
+++ Exercises/APIs/week2/pokemon-fetch/index.js
@@ -16,25 +16,18 @@ const VALID_URL = "https://pokeapi.co/api/v2/pokemon/?limit=5";
 const INVALID_URL = "https://pokeapi.co/api/v2/pokemons/?limit=5";
 
 async function fetchJSON(url) {
-  // TODO
-
-  // Fetch the JSON data from the web API that responds to the `url` parameter
-  // and return a promise that resolves to a corresponding JavaScript object.
-  // Make sure to check for HTTP errors.
-
-  let response = await fetch(url);
-
-  return new Promise((resolve, reject) => {
-    try {
+  try {
+    return await fetch(url).then((response) => {
       if (!response.ok) {
-        throw new Error(response.status);
+        throw new Error(
+          `Error happened while calling url. Status:${response.status}`
+        );
       }
-
-      resolve(response.json());
-    } catch (err) {
-      reject(err);
-    }
-  });
+      return response.json();
+    });
+  } catch (err) {
+    throw err;
+  }
 }
 
 function renderResults(pokemons) {
@@ -65,11 +58,6 @@ function main() {
   button.addEventListener("click", () => {
     const option = document.querySelector("#option");
     const url = option.checked ? INVALID_URL : VALID_URL;
-
-    // TODO
-    // Use `fetchJSON()` to fetch data from the selected url.
-    // If successful, render the data by calling function `renderResults()`.
-    // On failure, render the error by calling function `renderError()`.
 
     fetchJSON(url)
       .then((data) => renderResults(data))

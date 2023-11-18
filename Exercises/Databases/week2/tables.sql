b3ab63d81be7c964cae09772bbe18d96aee1b761
zehra <zehrakocairi@gmail.com>
2023-11-18 20:30:37 +0100

    some changes

--- Exercises/Databases/week2/tables.sql
+++ Exercises/Databases/week2/tables.sql
@@ -30,21 +30,3 @@ CREATE TABLE Ingredient (
     amount int,
     foreign key (step_id) REFERENCES Step(id)
 );
-
-select recipe.* from recipe
-left join recipe_category on recipe.id = recipe_category.recipe_id
-left join category on recipe_category.category_id = category.id
-left join step on  recipe.id = step.recipe_id 
-left join ingredient on step.id = ingredient.step_id
-where category.name = "Vegetarian" and ingredient.name = "Condensed milk",
-
-select recipe.* from recipe
-left join recipe_category on recipe.id = recipe_category.recipe_id
-left join category on recipe_category.category_id = category.id
-left join step on  recipe.id = step.recipe_id 
-where category.name = "Cake" and step.description not like "%bake%"
-
-select recipe.*  from recipe
-left join recipe_category on recipe.id = recipe_category.recipe_id
-left join category on recipe_category.category_id = category.id
-where category.name = "Vegan" or category.name = "Japanese" 
